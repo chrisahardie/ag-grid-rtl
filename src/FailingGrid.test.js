f9909a84ff07c637f11a4762edab1b1ebda86c5b
Christopher Hardie <chardie@lixar.com>
2020-10-28 09:31:10 -0400

    Added timeouts

--- src/FailingGrid.test.js
+++ src/FailingGrid.test.js
@@ -3,6 +3,6 @@ import FailingGrid from './FailingGrid';
 
 test('grid that fails test', async () => {
   render(<FailingGrid />);
-  const consigneeName = await screen.findByText(/hank/i);
+  const consigneeName = await screen.findByText(/hank/i, undefined, { timeout: 5000});
   expect(consigneeName).toBeInTheDocument();
-});
+}, 10000);
